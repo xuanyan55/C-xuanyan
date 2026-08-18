3030e4d3932fba613c817c94ed4ea9a4c0011813
xuanyan55 <xy2062194641@163.com>
2026-08-19 00:40:30 +0800

    Add files via upload

--- second.cpp
+++ second.cpp
@@ -1,13 +1,16 @@
-﻿#include <stdio.h>
-int main() {
-	int k;
-int n = 0;
-double sum = 0;
-scanf_s("%d",&k);
-while (sum <= k) {
-	n = n + 1;
-	sum = sum + 1.0 / n;
-}
-printf("n%d\n", n);
-return 0;
+﻿#include<stdio.h>
+int main(void) {
+	int apple[10];
+	int h;
+	int count = 0;
+	for (int i = 0; i < 10; i++) {
+		scanf("%d", &apple[i]);}
+	scanf("%d", &h);
+	for (int i = 0; i < 10; i++) {
+		if (apple[i] <= h+30)
+		{count++;
+		}
+	}
+	printf("%d", count);
+	return 0;
 }
