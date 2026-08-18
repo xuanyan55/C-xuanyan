eed9701c0bacd20083e0279db3051581bec1957e
xuanyan55 <xy2062194641@163.com>
2026-08-18 14:24:59 +0800

    Add files via upload

---
+++ second.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+int main() {
+	int k;
+int n = 0;
+double sum = 0;
+scanf_s("%d",&k);
+while (sum <= k) {
+	n = n + 1;
+	sum = sum + 1.0 / n;
+}
+printf("n%d\n", n);
+return 0;
+}
